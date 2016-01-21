c7e610abe66150ff3dbf7321c2336bf281ec713b
andrew ju <andrewjuey@gmail.com>
2016-01-21 15:18:30 -0800

    update 'web scraper v1.py'

--- web scraper v1.py
+++ web scraper v1.py
@@ -1,72 +1,80 @@
-# first attempt at web scraper for DSSG - 1/7/2016
-# using BS4 and requests
-# guide 1: http://journalistsresource.org/tip-sheets/research/python-scrape-website-data-criminal-justice
-# guide 2: http://blog.miguelgrinberg.com/post/easy-web-scraping-with-python
+# 1/16/2016
+# using BS4, requests
+# general guide: http://journalistsresource.org/tip-sheets/research/python-scrape-website-data-criminal-justice
 
 import requests
 from bs4 import BeautifulSoup
-import time
-
-url_scraped = 'http://www.jobaps.com/sf/default.asp#JobListing'
-
-r = requests.get(url_scraped)
-# print(r.text)
-
-soup = BeautifulSoup(r.text)
-# OR (r.content) ? (r.text) may not always work [what is parsing]
-# print(len(soup.select('#JobListing tr')[2:]))
 
 
-sfgov_job_listings = []
+def get_crawled_data():
+    data = []
 
-for list_item in soup.select("#JobListing tr")[2:]:
-    # print(list_item['class'])
-        # in HTML, # is table, . is list
+    # Load HTML into memory
+    url_scraped = 'http://www.jobaps.com/sf/default.asp#JobListing'
+    html = requests.get(url_scraped)
 
-    # if list_item['class'] == ['ColumnHeader']:
-    #     # print(list_item)
-    #     continue
+    # Convert HTML into tree
+    tree = BeautifulSoup(html.text)
 
-    if list_item['class'] == ['RowHeader']:
-        continue
+    # Find job listings
+    # TODO: Document the HTML structure you are extracting
+    table = tree.select("#JobListing tr")
+    # skip first two rows
+    rows = table[2:]
+    for row in rows:
+        # Skip section headers
+        if row['class'] == ['RowHeader']:
+            continue
 
-    # print(list_item)
+        # Get cells of data in each job row
+        cells = list(row.children)
 
-    list_cells = list(list_item.children)
+        job_title_cell = cells[1]
+        job_title_cell_text_sections = list(job_title_cell.children)
+        job_title = job_title_cell_text_sections[0].text
+        job_code = job_title_cell_text_sections[2].text
 
-    # print(list(list_item.children)[1])
-    # print(list(list_item.children)[2])
-    # print(list(list_item.children)[3])
+        dept = str(cells[3].string)
 
-    job_title = list(list_cells[1].children)[0].text
-    dept = list_cells[3].string
-# not .text
+        salary_ranges_text = str(cells[5].string).strip()
+        salary_ranges = salary_ranges_text.split('; ')
+        for salary_range_text in salary_ranges:
+            if salary_range_text[-5:] == '/year':
+                salary_range = salary_range_text[:-5]
+                print(salary_range)
 
-    print(job_title)
-    print(dept)
+        data.append({
+            'title': job_title,
+            'dept': dept,
+            'salary': salary_range
+        })
 
-    # dept = list(list_item.children)[2]
-
-#    list_cells = list_item.findAll('td')
-# # maybe wrong here? not sure what the soup.select pulls, but html here is odd/even, almost like overlaying structure is tr
-# # maybe use table_id instead? table_id is 'JobListing'
-#
-#     if len(list_cells) > 0:
-#         relative_link_to_sfgov_joblinks = list_cells[0].find('a')['href']
-#         absolute_link_to_sfgov_joblinks = url_to_scrape + relative_link_to_sfgov_joblinks
-#         sfgov_job_listings.append(absolute_link_to_sfgov_joblinks)
-#
-# sfgov_jobs = []
-#
-# for sfgov_vidslinks[:10]:
-#     # don't understand the [:10]:...
-#     r = requests.get(sfgov_vidslinks)
-#     soup = BeautifulSoup(r.text)
-#
-#     sfgov_details = {}
+    return data
 
 
+def get_analysis(data):
+    analysis = {}
+
+    analysis['num_total'] = len(data)
+    analysis['salary_avg'] = None
+
+    depts = {}
+    for job in data:
+        if not job['dept'] in depts:
+            depts[job['dept']] = 1
+        else:
+            depts[job['dept']] += 1
+    analysis['num_by_dept'] = depts
+
+    return analysis
 
 
+def main():
+    data = get_crawled_data()
+    print(data)
+    analysis = get_analysis(data)
+    print(analysis)
 
 
+if __name__ == '__main__':
+    main()
